8fba8a1dcdcb962b07ce02108639b9d26593241c
Lars Kellogg-Stedman <lars@oddbit.com>
2026-06-05 08:21:40 -0400

    Reverse joystick orientation

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,16 +3,16 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         TG(1),              // GP0 (Layer)
-        LGUI(KC_PAGE_UP),   // GP2 (Up)
-        LGUI(KC_PAGE_DOWN), // GP3 (Down)
-        LGUI(KC_LEFT),      // GP4 (Left)
-        LGUI(KC_RIGHT)      // GP5 (Right)
+        LGUI(KC_PAGE_DOWN), // GP2 (Down)
+        LGUI(KC_PAGE_UP),   // GP3 (Up)
+        LGUI(KC_RIGHT),     // GP4 (Right)
+        LGUI(KC_LEFT)       // GP5 (Left)
         ),
     [1] = LAYOUT(
         TG(1),              // GP0 (Layer)
-        KC_UP,              // GP2 (Up)
-        KC_DOWN,            // GP3 (Down)
-        KC_LEFT,            // GP4 (Left)
-        KC_RIGHT            // GP5 (Right)
+        KC_DOWN,            // GP2 (Down)
+        KC_UP,              // GP3 (Up)
+        KC_RIGHT,           // GP4 (Right)
+        KC_LEFT             // GP5 (Left)
         ),
 };
