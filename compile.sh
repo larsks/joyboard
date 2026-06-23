9a86fbe527bb1d19795470bb14969d6f407ac089
Lars Kellogg-Stedman <lars@oddbit.com>
2026-06-22 21:39:39 -0400

    build: Do not require pre-installed qmk_firmware directory

--- compile.sh
+++ compile.sh
@@ -1,4 +1,60 @@
-#!/bin/sh
+#!/bin/bash
+
+o_update=0
+
+while getopts u ch; do
+  case $ch in
+  u) o_update=1 ;;
+  \?) exit 2 ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 set -e
-qmk compile -kb joyboard -km default &&
-    cp "$(qmk env QMK_HOME)/joyboard_default.uf2" ./
+
+if type podman >&/dev/null; then
+  container_runtime=podman
+elif type docker >&/dev/null; then
+  container_runtime=docker
+else
+  echo "ERROR: no container runtime available" >&2
+  exit 1
+fi
+
+echo "## Using $container_runtime as container runtime"
+
+if ! [[ -d qmk_firmware ]]; then
+  cat <<EOF
+
+##
+## Cloning qmk_firmware repository.
+##
+
+EOF
+  git clone --filter blob:none --recursive https://github.com/qmk/qmk_firmware qmk_firmware
+fi
+
+if ((o_update)); then
+  cat <<EOF
+
+##
+## Updating qmk_firmware repository
+##
+
+EOF
+  git -C qmk_firmware pull
+fi
+
+cat <<EOF
+
+##
+## Building joyboard firmware
+##
+
+EOF
+"$container_runtime" run --rm -it \
+  -v "$PWD/qmk_firmware:/qmk_firmware:z" \
+  -v "$PWD:/qmk_firmware/keyboards/joyboard:z" \
+  -w /qmk_firmware \
+  ghcr.io/qmk/qmk_cli \
+  qmk compile -kb joyboard -km default
--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+if ! [[ -f qmk_firmware/joyboard_default.uf2 ]]; then
+  echo "joyboard firmware is missing (maybe you need to run compile.sh)" >&2
+  exit 1
+fi
 
 sudo mount LABEL=RPI-RP2 /mnt &&
-    sudo cp joyboard_default.uf2 /mnt
+  sudo cp qmk_firmware/joyboard_default.uf2 /mnt
 sudo umount /mnt
