5b34d2aa63a4cf5c204f723a9715355975f4f947
Lars Kellogg-Stedman <lars@oddbit.com>
2026-06-24 18:41:18 -0400

    docs: Add a comment about layer order
    (Because I forgot and was confused)

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,24 +1,27 @@
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-    HAT_UP = QK_USER_0,
-    HAT_DOWN,
-    HAT_LEFT,
-    HAT_RIGHT,
-    MICMUTE,
-    SAVE_LAYER,
-    GOTO_SAVED,
-    NEXT_LAYER,
-    PREV_LAYER,
-    SHOW_LAYER,
+  HAT_UP = QK_USER_0,
+  HAT_DOWN,
+  HAT_LEFT,
+  HAT_RIGHT,
+  MICMUTE,
+  SAVE_LAYER,
+  GOTO_SAVED,
+  NEXT_LAYER,
+  PREV_LAYER,
+  SHOW_LAYER,
 };
 
+// *** WARNING ***
+// Layer order is controlled *here*, not in `keymaps`.
+// *** WARNING ***
 enum layer_names {
-    LAYER_JOYSTICK = 0,
-    LAYER_NIRI,
-    LAYER_BROWSER,
-    LAYER_MAPS,
-    LAYER_ARROWS,
+  LAYER_JOYSTICK = 0,
+  LAYER_NIRI,
+  LAYER_BROWSER,
+  LAYER_MAPS,
+  LAYER_ARROWS,
 };
 
 #define NUM_LAYERS 5
@@ -36,94 +39,94 @@ combo_t key_combos[] = {
 };
 
 // Always resolve combos from layer 0.
-uint8_t combo_ref_from_layer(uint8_t layer) {
-    return 0;
-}
+uint8_t combo_ref_from_layer(uint8_t layer) { return 0; }
 
 void keyboard_post_init_user(void) {
-    // Restore active layer from flash
-    uint8_t saved_layer = eeconfig_read_user();
-    if (saved_layer > 0 && saved_layer < NUM_LAYERS) {
-        layer_move(saved_layer);
-    }
+  // Restore active layer from flash
+  uint8_t saved_layer = eeconfig_read_user();
+  if (saved_layer > 0 && saved_layer < NUM_LAYERS) {
+    layer_move(saved_layer);
+  }
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    joystick_set_hat(JOYSTICK_HAT_CENTER);
-    joystick_flush();
-    return state;
+  joystick_set_hat(JOYSTICK_HAT_CENTER);
+  joystick_flush();
+  return state;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // Toggle switches: let QMK handle layer keycodes; tap others once
-    if (record->event.key.col >= 10 && record->event.key.col <= 13) {
-        if (IS_QK_TO(keycode)) {
-            return true;
-        }
-        if (record->event.pressed) {
-            uint8_t current = get_highest_layer(layer_state);
-            if (keycode == SAVE_LAYER) {
-                eeconfig_update_user(current);
-            } else if (keycode == GOTO_SAVED) {
-                uint8_t saved = eeconfig_read_user();
-                if (saved < NUM_LAYERS) {
-                    layer_move(saved);
-                } else {
-                    layer_move(0);
-                }
-            } else if (keycode == NEXT_LAYER) {
-                layer_move((current + 1) % NUM_LAYERS);
-            } else if (keycode == PREV_LAYER) {
-                layer_move((current + NUM_LAYERS - 1) % NUM_LAYERS);
-            } else {
-                tap_code16(keycode);
-            }
-        }
-        return false;
+  // Toggle switches: let QMK handle layer keycodes; tap others once
+  if (record->event.key.col >= 10 && record->event.key.col <= 13) {
+    if (IS_QK_TO(keycode)) {
+      return true;
     }
-
-    if (!record->event.pressed) {
-        if (keycode == HAT_UP || keycode == HAT_DOWN || keycode == HAT_LEFT || keycode == HAT_RIGHT) {
-            joystick_set_hat(JOYSTICK_HAT_CENTER);
-            joystick_flush();
-            return false;
-        } else if (keycode == MICMUTE) {
-            host_system_send(0);
-            return false;
+    if (record->event.pressed) {
+      uint8_t current = get_highest_layer(layer_state);
+      if (keycode == SAVE_LAYER) {
+        eeconfig_update_user(current);
+      } else if (keycode == GOTO_SAVED) {
+        uint8_t saved = eeconfig_read_user();
+        if (saved < NUM_LAYERS) {
+          layer_move(saved);
+        } else {
+          layer_move(0);
         }
-        return true;
+      } else if (keycode == NEXT_LAYER) {
+        layer_move((current + 1) % NUM_LAYERS);
+      } else if (keycode == PREV_LAYER) {
+        layer_move((current + NUM_LAYERS - 1) % NUM_LAYERS);
+      } else {
+        tap_code16(keycode);
+      }
     }
+    return false;
+  }
 
-    switch (keycode) {
-        case HAT_UP:
-            joystick_set_hat(JOYSTICK_HAT_NORTH);
-            joystick_flush();
-            return false;
-        case HAT_DOWN:
-            joystick_set_hat(JOYSTICK_HAT_SOUTH);
-            joystick_flush();
-            return false;
-        case HAT_LEFT:
-            joystick_set_hat(JOYSTICK_HAT_WEST);
-            joystick_flush();
-            return false;
-        case HAT_RIGHT:
-            joystick_set_hat(JOYSTICK_HAT_EAST);
-            joystick_flush();
-            return false;
-        case MICMUTE:
-            host_system_send(SYSTEM_MICROPHONE_MUTE);
-            return false;
-        case SHOW_LAYER: {
-            const uint16_t digits[] = {KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9};
-            uint8_t        layer    = get_highest_layer(layer_state);
-            if (layer < 10) {
-                tap_code16(digits[layer]);
-            }
-            return false;
-        }
+  if (!record->event.pressed) {
+    if (keycode == HAT_UP || keycode == HAT_DOWN || keycode == HAT_LEFT ||
+        keycode == HAT_RIGHT) {
+      joystick_set_hat(JOYSTICK_HAT_CENTER);
+      joystick_flush();
+      return false;
+    } else if (keycode == MICMUTE) {
+      host_system_send(0);
+      return false;
     }
     return true;
+  }
+
+  switch (keycode) {
+  case HAT_UP:
+    joystick_set_hat(JOYSTICK_HAT_NORTH);
+    joystick_flush();
+    return false;
+  case HAT_DOWN:
+    joystick_set_hat(JOYSTICK_HAT_SOUTH);
+    joystick_flush();
+    return false;
+  case HAT_LEFT:
+    joystick_set_hat(JOYSTICK_HAT_WEST);
+    joystick_flush();
+    return false;
+  case HAT_RIGHT:
+    joystick_set_hat(JOYSTICK_HAT_EAST);
+    joystick_flush();
+    return false;
+  case MICMUTE:
+    host_system_send(SYSTEM_MICROPHONE_MUTE);
+    return false;
+  case SHOW_LAYER: {
+    const uint16_t digits[] = {KC_0, KC_1, KC_2, KC_3, KC_4,
+                               KC_5, KC_6, KC_7, KC_8, KC_9};
+    uint8_t layer = get_highest_layer(layer_state);
+    if (layer < 10) {
+      tap_code16(digits[layer]);
+    }
+    return false;
+  }
+  }
+  return true;
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -220,10 +223,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        tap_code16(clockwise ? LALT(KC_VOLU) : LALT(KC_VOLD));
-    } else {
-        tap_code16(clockwise ? KC_VOLU : KC_VOLD);
-    }
-    return false;
+  if (index == 0) {
+    tap_code16(clockwise ? LALT(KC_VOLU) : LALT(KC_VOLD));
+  } else {
+    tap_code16(clockwise ? KC_VOLU : KC_VOLD);
+  }
+  return false;
 }
